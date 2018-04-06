c4b22093367beca703bff0fb2d24b19093f7700f
Nicola Gheza <ghzncl@gmail.com>
2018-04-06 15:52:15 +0200

    Evolutionary Programming for Model Selection
    TPOT

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import pandas as pd
-import numpy as np
-from sklearn.neural_network import MLPRegressor
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from tpot import TPOTRegressor
+import numpy as np
 
 #Load data
 train_df = pd.read_csv('./competitions/ken3450/train.csv')
 test_df  = pd.read_csv('./competitions/ken3450/test.csv')
 
-X_train = train_df.drop("shares", axis=1)
-y_train = train_df["shares"]
+X = train_df.drop("shares", axis=1)
+y = train_df["shares"]
+X_train, X_test, y_train, y_test = train_test_split(X,y,train_size=0.7, test_size=0.3)
 
-pipeline = make_pipeline(StandardScaler(), MLPRegressor(activation='relu', 
-														verbose=True, max_iter=500))
-mlp_model=pipeline.fit(X_train, y_train)
+print(X_train.shape, type(X_train))
+print(y_train.shape, type(y_train))
+tpot = TPOTRegressor(generations=5, population_size=20, verbosity=2)
+tpot.fit(X_train, y_train)
+print(tpot.score(X_test, y_test))
+tpot.export('tpot_boston_pipeline.py')
+# df = pd.DataFrame({
+#         'id': pd.Series(range(1,9912)),
+#         'shares': pred
+# })
 
-pred = mlp_model.predict(test_df)
-
-df = pd.DataFrame({
-        'id': pd.Series(range(1,9912)),
-        'shares': pred
-})
-
-df.to_csv("sol.csv", index=False)
+# df.to_csv("sol.csv", index=False)
